d533634bd23527d4fcc95602262b29d70530d967
DanielGonzalezCasilimas <danigonzalezcasilima@gmail.com>
2021-08-09 22:59:54 -0500

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AplicacionBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que imprima 5 líneas de Texto");
+
+            Console.WriteLine("");
+            Console.WriteLine("Parte principales de un auto");
+
+            Console.WriteLine("");
+            Console.WriteLine("* Motor");
+
+            Console.WriteLine("");
+            Console.WriteLine("* Neumaticos");   
+
+            Console.WriteLine("");
+            Console.WriteLine("* Bateria");             
+
+            Console.WriteLine("");
+            Console.WriteLine("* Chasis");    
+
+            Console.WriteLine("");
+            Console.WriteLine("* Carroceria");   
+        }
+    }
+}
+
